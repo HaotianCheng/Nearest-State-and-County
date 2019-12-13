99d110091e726ca4ad046d7775eb795308741250
Gordon Wallace <gaw1@bu.edu>
2019-12-12 22:15:46 -0500

    Bug fixes

--- QuadTree/Nearest State/src/Project/RTree.java
+++ QuadTree/Nearest State/src/Project/RTree.java
@@ -16,20 +16,80 @@ public class RTree {
 	
 	public void search(Coord coord, Node[] nodes) {
 		RTree search_tree = this;
-		while(search_tree.node == null)
+		boolean found_box = false;
+		//while(search_tree.child1.topleft != null)
+		//{
+		for(int i = 0; i < 13; i++)
 		{
+			if(search_tree.child1.topleft != null) {
+				////System.out.println("Is it null?: " + search_tree.child1.topleft == null);
+				if(is_contained_by_coord(search_tree.child1, coord)) {
+					System.out.println("1");
+					found_box = true;
+					search_tree = search_tree.child1;
+				}
+			}
+			if(search_tree.child2.topleft != null) {
+				if(is_contained_by_coord(search_tree.child2, coord)) {
+					System.out.println("2");
+					found_box = true;
+					search_tree = search_tree.child2;
+				}
+			}
+			if(search_tree.child3 != null)
+			{
+			if (search_tree.child3.topleft != null) {
+				if(is_contained_by_coord(search_tree.child3, coord)) {
+					System.out.println("3");
+					found_box = true;
+					search_tree = search_tree.child3;
+				}
+			}
+			}
+			System.out.println("Found box: " + found_box);
+			System.out.println("Coordinate to search: " + coord.x + " " + coord.y);
+			if(found_box == false)
+			{
+				//Find which one's closest
+				//return;
+			}
 			if(debug_mode)
 			{
 				System.out.println("Top left: " + search_tree.topleft);
 				System.out.println("Bottom right: " + search_tree.bottomright);
+				//System.out.println("Top left: " + search_tree.child2.topleft);
+				//System.out.println("Bottom right: " + search_tree.child2.bottomright);
 			}
-			search_tree = search_tree.child1;
-			System.out.println(search_tree);
 		}
+		//}
+		RTree not_null[] = new RTree[3];
+		if(search_tree.child1 != null)
+		{
+			not_null[0] = search_tree.child1;
+		}
+		if(search_tree.child2 != null)
+		{
+			not_null[1] = search_tree.child2;
+		}
+		if(search_tree.child3 != null)
+		{
+			not_null[2] = search_tree.child3;
+		}
+		Node closest = closest_node(not_null, coord);
+		//System.out.println(closest);
+		//System.out.println(closest.point);
 		for (int i = 0; i < 10; i++)
 		{
-			nodes[i] = search_tree.node;
+			nodes[i] = closest;
 		}
+	}
+	
+	public Node closest_node(RTree[] children, Coord coord)
+	{
+		System.out.println(children[0].node);
+		System.out.println(children[0].child1.node);
+		System.out.println(children[0].child1.child1);
+		return children[0].node;
 	}
 	
 	public void print_tree(RTree tree)
@@ -93,12 +153,13 @@ public class RTree {
 	
 	public boolean insert(Node node) { //Boolean flags whether the root node has changed (1 means true)
 		boolean not_leaf = true;
+		expand_box(this, node);
 		if(this.child1 != null)
 		{
 			if(this.child1.node != null)
 			{
 				if(debug_mode) {
-				System.out.println("child1");
+				//System.out.println("child1");
 				}
 				not_leaf = false;
 			}
@@ -108,7 +169,7 @@ public class RTree {
 			if(this.child2.node != null)
 			{
 				if(debug_mode) {
-				System.out.println("child2");
+				//System.out.println("child2");
 				}
 				not_leaf = false;
 			}
@@ -118,7 +179,7 @@ public class RTree {
 			if(this.child3.node != null)
 			{
 				if(debug_mode) {
-				System.out.println("child3");
+				//System.out.println("child3");
 				}
 				not_leaf = false;
 			}
@@ -127,7 +188,7 @@ public class RTree {
 		if(this.child1 == null & this.child2 == null & this.child3 == null) //First insertion case
 		{
 			if(debug_mode) {
-			System.out.println("first");
+			//System.out.println("first");
 			}
 			this.child1 = new RTree();
 			this.child1.parent = this;
@@ -136,14 +197,14 @@ public class RTree {
 		else if(not_leaf) //Non-leaf case
 		{
 			if(debug_mode) {
-			System.out.println("Non-leaf case");
+			//System.out.println("Non-leaf case");
 			}
 			if(this.child1 != null)
 			{
 				if(is_contained_by(this.child1, node))
 				{
 					if(debug_mode) {
-					System.out.println("Recursion on child 1");
+					//System.out.println("Recursion on child 1");
 					}
 					this.child1.insert(node);
 					return false;
@@ -154,7 +215,7 @@ public class RTree {
 				if(is_contained_by(this.child2, node))
 				{
 					if(debug_mode) {
-					System.out.println("Recursion on child 2");
+					//System.out.println("Recursion on child 2");
 					}
 					this.child2.insert(node);
 					return false;
@@ -165,7 +226,7 @@ public class RTree {
 				if(is_contained_by(this.child3, node))
 				{
 					if(debug_mode) {
-					System.out.println("Recursion on child 3");
+					//System.out.println("Recursion on child 3");
 					}
 					this.child3.insert(node);
 					return false;
@@ -173,7 +234,7 @@ public class RTree {
 			}
 
 				if(debug_mode) {
-				System.out.println("Didn't fit into children.");
+				//System.out.println("Didn't fit into children.");
 				}
 				double a1 = area_added(this.child1, node);
 				double a2 = area_added(this.child2, node);
@@ -183,22 +244,22 @@ public class RTree {
 					if(a2 < a3)
 					{
 						if(debug_mode) {
-						System.out.println("Expanding child 2.");
+						//System.out.println("Expanding child 2.");
 						}
 						expand_box(this.child2, node);
 						if(debug_mode) {
-						System.out.println("Inserting into child 2.");
+						//System.out.println("Inserting into child 2.");
 						}
 						this.child2.insert(node);
 					}
 					else
 					{
 						if(debug_mode) {
-						System.out.println("Expanding child 3.");
+						//System.out.println("Expanding child 3.");
 						}
 						expand_box(this.child3, node);
 						if(debug_mode) {
-						System.out.println("Inserting into child 3.");
+						//System.out.println("Inserting into child 3.");
 						}
 						this.child3.insert(node);
 					}
@@ -208,22 +269,22 @@ public class RTree {
 					if(a1 < a3)
 					{
 						if(debug_mode) {
-						System.out.println("Expanding child 1.");
+						//System.out.println("Expanding child 1.");
 						}
 						expand_box(this.child1, node);
 						if(debug_mode) {
-						System.out.println("Inserting into child 1.");
+						//System.out.println("Inserting into child 1.");
 						}
 						this.child1.insert(node);
 					}
 					else
 					{
 						if(debug_mode) {
-						System.out.println("Expanding child 3.");
+						//System.out.println("Expanding child 3.");
 						}
 						expand_box(this.child3, node);
 						if(debug_mode) {
-						System.out.println("Inserting into child 3.");
+						//System.out.println("Inserting into child 3.");
 						}
 						this.child3.insert(node);
 					}
@@ -232,12 +293,12 @@ public class RTree {
 		else //Leaf case
 		{
 			if(debug_mode) {
-			System.out.println("Leaf case");
+			//System.out.println("Leaf case");
 			}
 			if(this.child1 == null)
 			{
 				if(debug_mode) {
-				System.out.println("Creating new child 1 tree and inserting the node.");
+				//System.out.println("Creating new child 1 tree and inserting the node.");
 				}
 				this.child1 = new RTree();
 				this.child1.parent = this;
@@ -246,7 +307,7 @@ public class RTree {
 			else if(this.child2 == null)
 			{
 				if(debug_mode) {
-				System.out.println("Creating new child 2 tree and inserting the node.");
+				//System.out.println("Creating new child 2 tree and inserting the node.");
 				}
 				this.child2 = new RTree();
 				this.child2.parent = this;
@@ -255,7 +316,7 @@ public class RTree {
 			else if(this.child3 == null)
 			{
 				if(debug_mode) {
-				System.out.println("Creating new child 3 tree and inserting the node.");
+				//System.out.println("Creating new child 3 tree and inserting the node.");
 				}
 				this.child3 = new RTree();
 				this.child3.parent = this;
@@ -264,13 +325,13 @@ public class RTree {
 			else //Full
 			{
 				if(debug_mode) {
-				System.out.println("Tree is full.  Inserting into overflow.");
+				//System.out.println("Tree is full.  Inserting into overflow.");
 				}
 				this.overflow = new RTree();
 				this.overflow.parent = this;
 				this.overflow.node = node;
 				if(debug_mode) {
-				System.out.println("Splitting tree.");
+				//System.out.println("Splitting tree.");
 				}
 				return split_tree(this);
 			}
@@ -283,6 +344,16 @@ public class RTree {
 		if(node.point.x >= tree.topleft.x & node.point.x <= tree.bottomright.x)
 		{
 			if(node.point.y <= tree.topleft.y & node.point.y >= tree.bottomright.y)
+				return true;
+		}
+		return false;
+	}
+	
+	public boolean is_contained_by_coord(RTree tree, Coord coord)
+	{
+		if(coord.x >= tree.topleft.x & coord.x <= tree.bottomright.x)
+		{
+			if(coord.y <= tree.topleft.y & coord.y >= tree.bottomright.y)
 				return true;
 		}
 		return false;
@@ -321,6 +392,18 @@ public class RTree {
 	
 	public void expand_box(RTree tree, Node node)
 	{	
+		if(tree.topleft == null)
+		{
+			tree.topleft = new Coord(0,0);
+			tree.topleft.x = node.point.x;
+			tree.topleft.y = node.point.y;
+		}
+		if(tree.bottomright == null)
+		{
+			tree.bottomright = new Coord(0,0);
+			tree.bottomright.x = node.point.x;
+			tree.bottomright.y = node.point.y;
+		}
 		if(tree.topleft.x > node.point.x)
 		{
 			tree.topleft.x = node.point.x;
@@ -329,14 +412,22 @@ public class RTree {
 		{
 			tree.topleft.y = node.point.y;
 		}
-		if(tree.bottomright.x < node.point.x);
+		//System.out.println(tree.bottomright.x);
+		if(tree.bottomright.x < node.point.x)
 		{
+			//System.out.println("What?");
+			//System.out.println("x" + tree.bottomright.x);
+			//System.out.println("x" + node.point.x);
 			tree.bottomright.x = node.point.x;
 		}
+		//System.out.println(tree.bottomright.x);
 		if(tree.bottomright.y > node.point.y)
 		{
+			//System.out.println(tree.topleft.y);
 			tree.bottomright.y = node.point.y;
+			//System.out.println(tree.topleft.y);
 		}
+		//System.out.println(tree.bottomright.x);
 	}
 	
 	public void recalc_box(RTree tree)
@@ -406,8 +497,10 @@ public class RTree {
 			}
 		}
 		
-		tree.topleft = new Coord(min_x, max_y);
-		tree.bottomright = new Coord(max_x, min_y);
+		Coord left = new Coord(min_x, max_y);
+		Coord right = new Coord(max_x, min_y);
+		tree.topleft = left;
+		tree.bottomright = right;
 	}
 	
 	
@@ -425,7 +518,7 @@ public class RTree {
 			if(tree.parent == null)
 			{
 				if(debug_mode) {
-				System.out.println("Creating new root.");
+				//System.out.println("Creating new root.");
 				}
 				RTree new_root = new RTree();
 				tree.parent = new_root;
@@ -438,28 +531,28 @@ public class RTree {
 			else if(tree.parent.child1 == null)
 			{
 				if(debug_mode) {
-				System.out.println("Inserting new tree as parent's child 1");
+				//System.out.println("Inserting new tree as parent's child 1");
 				}
 				tree.parent.child1 = new_tree;
 			}
 			else if(tree.parent.child2 == null)
 			{
 				if(debug_mode) {
-				System.out.println("Inserting new tree as parent's child 2");
+				//System.out.println("Inserting new tree as parent's child 2");
 				}
 				tree.parent.child2 = new_tree;
 			}
 			else if(tree.parent.child3 == null)
 			{
 				if(debug_mode) {
-				System.out.println("Inserting new tree as parent's child 3");
+				//System.out.println("Inserting new tree as parent's child 3");
 				}
 				tree.parent.child3 = new_tree;
 			}
 			else
 			{
 				if(debug_mode) {
-				System.out.println("Inserted at parent. Parent overflowed and will now be split.");
+				//System.out.println("Inserted at parent. Parent overflowed and will now be split.");
 				}
 				tree.parent.overflow = new_tree;
 				split_tree(tree.parent);
